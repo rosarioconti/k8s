d822dc211920bcf129c9bfdaf49b77e4173eff68
Rosario Conti <rosabnamro@gmail.com>
2019-11-06 09:32:26 +0100

    Adding docker ignore and passing also the docker env message to the output

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3002
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var message = process.env.MESSAGE + " Hello world!";
+
+app.get('/', (req, res) => res.send(message))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
